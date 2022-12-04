9ea6a5625d675a1c316d7d53f0ca2046c882447e
Stanislaw Cruz <stanycruz@outlook.com>
2022-12-04 12:14:03 -0300

    refactor: update code

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwkoIRmmMBj4nkP72ePD26kOFeYeqOeEo",
-  authDomain: "house-marketplace-app-6216e.firebaseapp.com",
-  projectId: "house-marketplace-app-6216e",
-  storageBucket: "house-marketplace-app-6216e.appspot.com",
-  messagingSenderId: "721930718776",
-  appId: "1:721930718776:web:ac723bdcf218a0dbfce0cd",
-};
+  apiKey: 'AIzaSyCwkoIRmmMBj4nkP72ePD26kOFeYeqOeEo',
+  authDomain: 'house-marketplace-app-6216e.firebaseapp.com',
+  projectId: 'house-marketplace-app-6216e',
+  storageBucket: 'house-marketplace-app-6216e.appspot.com',
+  messagingSenderId: '721930718776',
+  appId: '1:721930718776:web:ac723bdcf218a0dbfce0cd',
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
