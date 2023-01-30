49a5c4474a523c7e6d321aebf10aff10aa558572
Stanislaw Cruz <stanycruz@outlook.com>
2023-01-30 19:04:59 -0300

    feat: add iser sign in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -20,6 +21,27 @@ function SignIn() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {      
+      const auth = getAuth()
+  
+      const userCredencial = await signInWithEmailAndPassword
+      (
+        auth,
+        email,
+        password
+      )
+  
+      if (userCredencial.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -29,7 +51,7 @@ function SignIn() {
           </p>
         </header>
 
-        <form action=''>
+        <form onSubmit={onSubmit}>
           <input 
             type='email' 
             className='emailInput' 
