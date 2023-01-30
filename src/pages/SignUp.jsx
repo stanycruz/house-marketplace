186a16d559afe5a5d5dc9adc6729c307946de9a8
Stanislaw Cruz <stanycruz@outlook.com>
2023-01-30 18:18:32 -0300

    feat: register user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +23,31 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+
+      const userCredencial = await createUserWithEmailAndPassword
+      (
+        auth,
+        email,
+        password
+      )
+
+      const user = userCredencial.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className='pageContainer'>
@@ -30,7 +57,7 @@ function SignUp() {
           </p>
         </header>
 
-        <form action=''>
+        <form onSubmit={onSubmit}>
           <input 
             type='text' 
             className='nameInput' 
