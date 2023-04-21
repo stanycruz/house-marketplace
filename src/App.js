907a6fb1d557bd45321cb96348edcb4d167b1c27
Stanislaw Cruz <stanycruz@outlook.com>
2023-04-21 12:01:07 -0300

    feat: add explore page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import Offers from './pages/Offers'
+import Category from './pages/Category'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -18,6 +19,7 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
