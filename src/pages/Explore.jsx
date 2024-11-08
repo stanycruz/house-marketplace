6849855c5b1a4f58f79cf3c3d5889d434cdd711a
Stanislaw Cruz <stanycruz@outlook.com>
2024-11-08 19:43:08 -0300

    feat: listing item component

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -4,31 +4,31 @@ import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 function Explore() {
   return (
-    <div className='explore'>
+    <div className="explore">
       <header>
-        <p className='pageHeader'>Explore</p>
+        <p className="pageHeader">Explore</p>
       </header>
 
       <main>
         {/* Slider */}
 
-        <p className='exploreCategoryHeading'>Categories</p>
-        <div className='exploreCategories'>
-          <Link to='/category/rent'>
-            <img 
-              src={rentCategoryImage} 
-              alt='rent' 
-              className='exploreCategoryImg' 
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
             />
-            <p className='exploreCategoryName'>Places for rent</p>
+            <p className="exploreCategoryName">Places for rent</p>
           </Link>
-          <Link to='/category/sell'>
-            <img 
-              src={sellCategoryImage} 
-              alt='rent' 
-              className='exploreCategoryImg' 
+          <Link to="/category/sale">
+            <img
+              src={sellCategoryImage}
+              alt="sale"
+              className="exploreCategoryImg"
             />
-            <p className='exploreCategoryName'>Places for sale</p>
+            <p className="exploreCategoryName">Places for sale</p>
           </Link>
         </div>
       </main>
