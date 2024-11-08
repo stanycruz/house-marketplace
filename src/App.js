92d04e1dbcda0355fe79f6b538fae75ff7384248
Stanislaw Cruz <stanycruz@outlook.com>
2024-11-08 20:20:31 -0300

    feat: add offers page

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Explore />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/offers' element={<Offers />} />
-          <Route path='/category/:categoryName' element={<Category />} />
-          <Route path='/profile' element={<PrivateRoute />}>
-            <Route path='/profile' element={<Profile />} />
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
           </Route>
-          <Route path='/sign-in' element={<SignIn />} />
-          <Route path='/sign-up' element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
