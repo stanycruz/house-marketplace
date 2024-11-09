a6e21373a81a447b8fa4a17435ad1a7211581829
Stanislaw Cruz <stanycruz@outlook.com>
2024-11-09 13:03:42 -0300

    feat: start create listing page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
-import {  useState } from 'react'
+import { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
   const auth = getAuth()
@@ -27,13 +29,13 @@ function Profile() {
       if (auth.currentUser.displayName !== name) {
         // Update didplay name in firebase
         await updateProfile(auth.currentUser, {
-          displayName: name
+          displayName: name,
         })
 
         // Update in firestore
         const userRef = doc(db, 'users', auth.currentUser.uid)
         await updateDoc(userRef, {
-          name
+          name,
         })
       }
     } catch (error) {
@@ -49,48 +51,56 @@ function Profile() {
     }))
   }
 
-  return <div className='profile'>
-      <header className='profileHeader'>
-        <p className='pageHeader'>My Profile</p>
-        <button type='button' className='logOut' onClick={onLogout}>
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
           Logout
         </button>
       </header>
       <main>
-        <div className='profileDetailsHeader'>
-          <p className='profileDetailsText'>Personal Details</p>
-          <p className='changePersonalDetails' onClick={() => {
-            changeDetails && onSubmit()
-            setChangeDetails((prevState) => !prevState)
-          }}>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails((prevState) => !prevState)
+            }}
+          >
             {changeDetails ? 'done' : 'change'}
           </p>
         </div>
 
-        <div className='profileCard'>
+        <div className="profileCard">
           <form>
-            <input 
-              type='text' 
-              id='name' 
-              className={!changeDetails ? 'profileName' : 
-              'profileNameActive'} 
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
               disabled={!changeDetails}
               value={name}
               onChange={onChange}
             />
-            <input 
-              type='text' 
-              id='email' 
-              className={!changeDetails ? 'profileEmail' : 
-              'profileEmailActive'} 
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
               disabled={!changeDetails}
               value={email}
               onChange={onChange}
             />
           </form>
         </div>
+
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+        </Link>
       </main>
     </div>
+  )
 }
 
 export default Profile
