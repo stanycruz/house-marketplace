047a8ce8fa7d2eba53dce0d9cdb5eead583c766b
Stanislaw Cruz <stanycruz@outlook.com>
2024-11-10 12:17:26 -0300

    feat: add profile listings & delete

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -42,11 +42,11 @@ function Listing() {
   return (
     <main>
       <Swiper slidesPerView={1} pagination={{ clickable: true }}>
-        {listing.imgUrls.map((url, index) => (
+        {listing.imageUrls.map((url, index) => (
           <SwiperSlide key={index}>
             <div
               style={{
-                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                background: `url(${listing.imageUrls[index]}) center no-repeat`,
                 backgroundSize: 'cover',
               }}
               className="swiperSlideDiv"
